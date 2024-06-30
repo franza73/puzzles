4e2be1a72ee433f764d1bcd9902d7c1a04cd5316
Franciraldo Cavalcante <franza@Franciraldos-MacBook-Pro.local>
2024-06-29 21:48:33 -0600

    Ponder This June 2024

--- 2024/ibm_jun_2024.py
+++ 2024/ibm_jun_2024.py
@@ -1,29 +1,33 @@
-# ---------------------------------------------------------------------------
-# We can use the equivalent representation (for m > n) for the pitagorean
-# triples:
-# a = m**2 - n**2
-# b = 2*m*n
-# c = m**2 + n**2
-# and the problem specifies a/b = pi.
-# So our problem now is to find a fractional x (i.e. m/n) that solves the
-# equation x/2 + 0.5/x = pi, with the correct precision. We solve the
-# equation by applying the Stern-Brocott method.
-# ---------------------------------------------------------------------------
-from mpmath import mp
+
+'''
+  We can use the equivalent representation (for m > n) for the pitagorean
+triples:
+  a = m**2 - n**2
+  b = 2*m*n
+  c = m**2 + n**2
+and the problem specifies a/b = pi.
+  So our problem now is to find a fractional x (i.e. m/n) that solves the
+equation x/2 + 0.5/x = pi, with the correct precision. We solve the
+equation by applying the Stern-Brocott method.
+'''
 from math import gcd
+from mpmath import mp
 
 
 def approx(val, delta):
-    a, b = mp.mpf('0'), mp.mpf('1')
-    c, d = mp.mpf('1'), mp.mpf('0')
+    '''
+    Use Stern-Brocott approximation to solve the equation.
+    '''
+    _a, _b = mp.mpf('0'), mp.mpf('1')
+    _c, _d = mp.mpf('1'), mp.mpf('0')
     while True:
-        m, n = a+c, b+d
+        m, n = _a+_c, _b+_d
         x = m/n
         fx = x/mp.mpf('2.0') - mp.mpf('0.5')/x
         if fx < val:
-            a, b = m, n
+            _a, _b = m, n
         elif fx > val:
-            c, d = m, n
+            _c, _d = m, n
         eps = mp.fabs(fx - val)
         if eps < mp.mpf('10')**(-delta):
             m, n = int(m), int(n)
@@ -38,12 +42,12 @@ def approx(val, delta):
 if __name__ == "__main__":
     mp.dps = 300
 
-    a, b, c = approx(mp.pi, 20)
-    print(f'With max number of digits {len(str(c))}:')
-    print(f'A = {a}\nB = {b}\nC = {c}\neps = ', end='')
-    mp.nprint(mp.fabs(mp.mpf(a)/mp.mpf(b) - mp.pi), 3)
+    A, B, C = approx(mp.pi, 20)
+    print(f'With max number of digits {len(str(C))}:')
+    print(f'A = {A}\nB = {B}\nC = {C}\neps = ', end='')
+    mp.nprint(mp.fabs(mp.mpf(A)/mp.mpf(B) - mp.pi), 3)
 
-    d, e, f = approx(mp.pi, 95)
-    print(f'With max number of digits {len(str(f))}:')
-    print(f'D = {d}\nE = {e}\nF = {f}\neps = ', end='')
-    mp.nprint(mp.fabs(mp.mpf(d)/mp.mpf(e) - mp.pi), 3)
+    D, E, F = approx(mp.pi, 95)
+    print(f'With max number of digits {len(str(F))}:')
+    print(f'D = {D}\nE = {E}\nF = {F}\neps = ', end='')
+    mp.nprint(mp.fabs(mp.mpf(D)/mp.mpf(E) - mp.pi), 3)
