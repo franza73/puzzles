214ad6228dcdd7341bd5a9a21b33313c5c60a4af
Franciraldo Cavalcante <franza73@users.noreply.github.com>
2024-12-10 06:29:10 -0700

    Update ibm_nov_2024.py

--- 2024/ibm_nov_2024.py
+++ 2024/ibm_nov_2024.py
@@ -65,13 +65,13 @@ def calculate_f_(args):
 if __name__ == '__main__':
     N = 600
     # goal = 144*3**2
-    goal = 128
-    # goal = 655
+    # goal = 128
+    goal = 655
     with Pool(os.cpu_count()) as pool:
         #for a in range(1, M+1):
         #    for b in range(1, a+1):
         #        for c in range(max(1, a-b), b+1):
-        for a in range(64, N):
+        for a in range(2, N):
             print(a, flush=True)
             for b in range(a, N):
                 for c in range(max(b - a, b), min(N - a - b, b + a)):
