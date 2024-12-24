1d48d3338dda5bf41bdf526ba70b361ce29c32af
Franciraldo Cavalcante <franza@Franciraldos-MacBook-Pro.local>
2024-12-23 20:26:59 -0700

    Ponder This Dec 2024 - star

--- 2024/ibm_dec_2024_star_.py
+++ 2024/ibm_dec_2024_star_.py
@@ -12,9 +12,22 @@ def function(n):
         return function(10**(k-1)-1) + r + 1
     return (f-1)*function(10**(k-1)-1) + 10**(k-1)  + function(r)
 
+# Make sure the function is correct for the example provided
 n = 1456
-#n = 1062880
-n = 13947137600
-#n = 99999999999999
-# 728
-print(function(n), n)
+assert function(1456) == 728
+
+# Find the range where the solution should be found
+for k in range(2, 15):
+    n = 10**k - 1
+    print(k, n, function(n)/ n)
+
+# Apply binary search to find the larger solution
+a, b = 10**13-1, 10**14-1
+while a < b:
+    m = (a + b) // 2
+    if 4*function(m) < 3*m:
+        a = m + 1 
+    else:
+        b = m - 1
+assert 4*function(m) == 3*m
+print(m)
