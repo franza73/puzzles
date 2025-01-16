26080c704559d5d5a2180815a8805991527dae36
Franciraldo Cavalcante <franza@Franciraldos-MacBook-Pro.local>
2025-01-15 17:57:51 -0700

    Ponder This Jan 2025

--- 2025/ibm_jan_2025.py
+++ 2025/ibm_jan_2025.py
@@ -2,19 +2,69 @@
 Solution:
 39 TC CA TC CA AS CA TC CA TC AB CA TC CA AS CA TC CA AS CA TC CA TC CA
    AS CA TC CA TC CA AS CA TC CA AS CA TC CA TC CA
-In 258.62s
 
 Solution to the bonus question:
 11 TA AB AC TA AC CS AC CS AC TA AC
 Vc = 4224/3187
-In additional 113.13s
 '''
 from math import sqrt
 from collections import deque
 
 
-def solve(va, vb, vc, eps):
-    def neigh(state):
+class Jug:
+    ''' Jug class that can be used to simulate the state of the problem '''
+    def __init__(self, v0, v1, v2, _vc):
+        self.v = (v0, v1, v2)
+        self.vc = _vc
+
+    def __add__(self, other):
+        return Jug(self.v[0] + other.v[0], self.v[1] + other.v[1],
+                   self.v[2] + other.v[2], self.vc)
+
+    def __sub__(self, other):
+        return Jug(self.v[0] - other.v[0],
+                   self.v[1] - other.v[1], self.v[2] - other.v[2], self.vc)
+
+    def __str__(self):
+        return str(self.v)
+
+    def eval(self):
+        ''' Numerical value for the jug content '''
+        return self.v[0]*sqrt(5) + self.v[1]*sqrt(3) + self.v[2]*self.vc
+
+    def __lt__(self, other):
+        if self.eval() < other.eval():
+            return True
+        return False
+
+    def __ne__(self, other):
+        if isinstance(other, int):
+            return self.eval() != other
+        return self.eval() != other.eval()
+
+    def __eq__(self, other):
+        return self.eval() == other.eval()
+
+    def __hash__(self):
+        return hash(self.v)
+
+
+def _reconstruct_path(sol, prev):
+    res = []
+    while sol in prev:
+        res.append(prev[sol][0])
+        sol = prev[sol][1]
+    return len(res), ' '.join(res[::-1])
+
+
+def _min_dist_to_one(state):
+    a, b, c = state
+    return min(abs(a.eval() - 1), abs(b.eval() - 1), abs(c.eval() - 1))
+
+
+def solve(_vc, eps):
+    ''' Solve the problem with specified precision '''
+    def _neigh(state):
         a, b, c = state
         res = []
         if a < va:
@@ -24,20 +74,24 @@ def solve(va, vb, vc, eps):
         if c < vc:
             res += [('TC', (a, b, vc))]
         if a != 0:
-            res += [('AS', (0, b, c))]
-            res += [('AB', (max(0, a + b - vb), min(a + b, vb), c))]
-            res += [('AC', (max(0, a + c - vc), b, min(a + c, vc)))]
+            res += [('AS', (z, b, c))]
+            res += [('AB', (max(z, a + b - vb), min(a + b, vb), c))]
+            res += [('AC', (max(z, a + c - vc), b, min(a + c, vc)))]
         if b != 0:
-            res += [('BS', (a, 0, c))]
-            res += [('BA', (min(a + b, va), max(0, a + b - va), c))]
-            res += [('BC', (a, max(0, b + c - vc), min(b + c, vc)))]
+            res += [('BS', (a, z, c))]
+            res += [('BA', (min(a + b, va), max(z, a + b - va), c))]
+            res += [('BC', (a, max(z, b + c - vc), min(b + c, vc)))]
         if c != 0:
-            res += [('CS', (a, b, 0))]
-            res += [('CA', (min(a + c, va), b, max(0, a + c - va)))]
-            res += [('CB', (a, min(b + c, vb), max(0, b + c - vb)))]
+            res += [('CS', (a, b, z))]
+            res += [('CA', (min(a + c, va), b, max(z, a + c - va)))]
+            res += [('CB', (a, min(b + c, vb), max(z, b + c - vb)))]
         return res
 
-    todo = deque([(0, (0, 0, 0))])
+    va = Jug(1, 0, 0, _vc)
+    vb = Jug(0, 1, 0, _vc)
+    vc = Jug(0, 0, 1, _vc)
+    z = Jug(0, 0, 0, _vc)
+    todo = deque([(0, (z, z, z))])
     visited = set()
     prev = {}
     sol = ()
@@ -45,24 +99,21 @@ def solve(va, vb, vc, eps):
         depth, state = todo.popleft()
         if state in visited:
             continue
-        a, b, c = state
-        if min(abs(a - 1), abs(b - 1), abs(c - 1)) < eps:
+        if _min_dist_to_one(state) < eps:
             sol = state
             break
         visited.add(state)
-        for move, n in neigh(state):
+        for move, n in _neigh(state):
             if n in visited:
                 continue
             if n not in prev:
                 prev[n] = [move, state]
             todo += [(depth+1, n)]
-    res = []
-    while sol in prev:
-        res.append(prev[sol][0])
-        sol = prev[sol][1]
-    return len(res), ' '.join(res[::-1])
+    return _reconstruct_path(sol, prev)
 
-def approx(i, j, k, val, delta):
+
+def approx(i, j, k, val, eps):
+    ''' Approximation to find fractional vc '''
     a_n, a_d = 0, 1
     b_n, b_d = 1, 0
     while True:
@@ -78,32 +129,37 @@ def approx(i, j, k, val, delta):
             if fx > val:
                 a_n, a_d = m, n
             elif fx < val:
-                b_n, b_d = m, n            
-        eps = abs(fx - val)
-        if eps < 10**(-delta):
+                b_n, b_d = m, n
+        if abs(fx - val) < eps:
             m, n = int(m), int(n)
             return m, n
-        
-if __name__ == "__main__":
-    va, vb, vc = sqrt(5), sqrt(3), sqrt(2)
 
+
+def main():
+    ''' Solve both parts of the problem '''
     print('Reproduce the example:')
-    print(solve(va, vb, vc, 0.01))
-    
+    print(solve(sqrt(2), 0.01))
+
     print('Solution:')
-    print(solve(va, vb, vc, 0.0003))
-    
+    print(solve(sqrt(2), 0.0003))
+
     print('Bonus question:')
+    eps = 10**(-8)
     for i in range(-10, 11):
         for j in range(-10, 11):
             for k in range(-10, 11):
-                if k == 0: continue
+                if k == 0:
+                    continue
                 vc = (1 - i*sqrt(5) - j*sqrt(3)) / k
                 m = abs(i) + abs(j) + abs(k)
                 if vc < 0 or vc > sqrt(3) or m > 11:
                     continue
-                if solve(va, vb, vc, 10**(-8))[0] == 11:
-                    vc_n, vc_d = approx(i, j, k, 1, 8) 
+                if solve(vc, eps)[0] == 11:
+                    vc_n, vc_d = approx(i, j, k, 1, eps)
                     print(i, j, k, (vc_n, vc_d))
-                    print(solve(va, vb, vc_n/vc_d, 10**(-8)))
+                    print(solve(vc_n/vc_d, eps))
                     print('---------')
+
+
+if __name__ == "__main__":
+    main()
