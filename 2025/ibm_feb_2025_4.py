e5e4cb8f9fc536ba753d30997347356854180d34
Franciraldo Cavalcante <franza@Franciraldos-MacBook-Pro.local>
2025-02-27 07:03:24 -0700

    run for n = 6 min symetric + shuffle (34)

--- 2025/ibm_feb_2025_4.py
+++ 2025/ibm_feb_2025_4.py
@@ -125,7 +125,7 @@ def solve(n):
         # exit(1)
         todo = []
         for a in sorted(H.keys()):
-            if a != 22:
+            if a != 34:
                 continue
             primes = H[a]
             trie = PrimesTrie(primes)
